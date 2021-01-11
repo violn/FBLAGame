30db1088fbbe115c8af60ee3ed2b0e1b783ad7c9
niol <32370882+violn@users.noreply.github.com>
2021-01-11 00:00:47 -0700

    Add files via upload

---
+++ ChangeOpacity.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class ChangeOpacity : MonoBehaviour
+{
+    private Renderer rend;
+
+    private Color AssetColor = new Color();
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        AssetColor.a = .5f;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (MoveUp())
+        {
+            rend.material.color = AssetColor;
+            if (AssetColor.a == 1f)
+            {
+                AssetColor.a = .5f;
+            }
+        }
+
+        else if (MoveDown())
+        {
+            rend.material.color = AssetColor;
+            if (AssetColor.a != 1f)
+            {
+                AssetColor.a = 1f;
+            }
+        }
+    }
+
+    private static bool MoveUp()
+    {
+        return Input.GetAxis("Vertical") > 0;
+    }
+
+    private static bool MoveDown()
+    {
+        return Input.GetAxis("Vertical") < 0;
+    }
+}
---
+++ Move2D.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class Move2D : MonoBehaviour
+{
+    private static float distance = 7f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 movement = new Vector3(0f, 0f, 0f);
+        if(MoveLeft())
+        {
+            movement.x = distance;
+            transform.position += movement * Time.deltaTime;
+        }
+
+        else if(MoveRight())
+        {
+            movement.x = distance * -1;
+            transform.position += movement * Time.deltaTime;
+        }
+    }
+
+    private static bool MoveLeft()
+    {
+        return Input.GetAxis("Horizontal") > 0;
+    }
+
+    private static bool MoveRight()
+    {
+        return Input.GetAxis("Horizontal") < 0;
+    }
+}
