0c45e84b05d07a4f6abcb2e2076cabf3c6900a69
niol <32370882+violn@users.noreply.github.com>
2021-01-11 21:22:37 -0700

    Update Move2D.cs

--- Assets/Player/Move2D.cs
+++ Assets/Player/Move2D.cs
@@ -1,42 +1,47 @@
-﻿using UnityEngine;
+using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
+using Player.Props;
 
-//A controller that allows the character to move
-//NOTE: in this game the charater movement has to be restricted somewhat
-//As the player will be moving through a grid but the frid won't be seen
-//This is the end for the psuedo-code as this script is unfinished
+//Controls how the player moves
 public class Move2D : MonoBehaviour
 {
-    private static float distance = 7f;
+    public Transform MoveHere;
+    public float Speed;
+    public LayerMask Colliders;
+
     // Start is called before the first frame update
     private void Start()
     {
+        MoveHere.parent = null;
     }
 
     // Update is called once per frame
     private void Update()
     {
-        Vector3 movement = new Vector3(0f, 0f, 0f);
-        if(MoveLeft())
+        //Causes the player to move towards our MoveHere
+        transform.position = Vector3.MoveTowards(transform.position, MoveHere.position, Speed * Time.deltaTime);
+
+        //The player can only move if they're directly on move here
+        if (Vector3.Distance(transform.position, MoveHere.position) <= .0f)
         {
-            movement.x = distance;
-            transform.position += movement * Time.deltaTime;
+            //Checks for the left or right input
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            {
+                //Checks for collision boxes to prevent phasing through walls
+                if(!Physics2D.OverlapCircle(MoveHere.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .5f, Colliders))
+                {
+                    //Move the player to the MoveHere
+                    MoveHere.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
+            }
+
+            //Moves up if in front of a box that doesn't have a box on top of it
+            //Checks for the up input
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f && PlayerProps.InFront)
+            {
+                //Move player to MoveHere
+                MoveHere.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            }
         }
-
-        else if(MoveRight())
-        {
-            movement.x = distance * -1;
-            transform.position += movement * Time.deltaTime;
-        }
-    }
-
-    private static bool MoveLeft()
-    {
-        return Input.GetAxis("Horizontal") > 0;
-    }
-
-    private static bool MoveRight()
-    {
-        return Input.GetAxis("Horizontal") < 0;
     }
 }
