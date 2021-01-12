44c65895b322ea5b0b0b0514c5d6176d94d0a2f2
niol <32370882+violn@users.noreply.github.com>
2021-01-11 21:23:02 -0700

    Add files via upload

---
+++ Assets/Player/PlayerProps.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Player.Props
+{
+    public class PlayerProps : MonoBehaviour
+    {
+        public static int Depth { set; get; }
+        public static bool InFront { get; set; } = false;
+        private void Start()
+        {
+            Depth = 0;
+        }
+    }
+}
