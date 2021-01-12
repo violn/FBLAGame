34e56e8d38a62a1064d1d6d8c4cef4f85c2f31df
niol <32370882+violn@users.noreply.github.com>
2021-01-11 21:33:39 -0700

    Update Move2D.cs

--- Assets/Player/Move2D.cs
+++ Assets/Player/Move2D.cs
@@ -1,12 +1,13 @@
 using UnityEngine;
 using Vector3 = UnityEngine.Vector3;
-//Controls how the player moves
-//Currently doesn't take in collission
+using Player.Props;
 
+//Controls how the player moves
 public class Move2D : MonoBehaviour
 {
     public Transform MoveHere;
     public float Speed;
+    public LayerMask Colliders;
 
     // Start is called before the first frame update
     private void Start()
@@ -17,19 +18,28 @@ public class Move2D : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        //Causes the player to move towards our move here
+        //Causes the player to move towards our MoveHere
         transform.position = Vector3.MoveTowards(transform.position, MoveHere.position, Speed * Time.deltaTime);
 
         //The player can only move if they're directly on move here
         if (Vector3.Distance(transform.position, MoveHere.position) <= .0f)
         {
+            //Checks for the left or right input
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                MoveHere.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                //Checks for collision boxes to prevent phasing through walls
+                if(!Physics2D.OverlapCircle(MoveHere.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .5f, Colliders))
+                {
+                    //Move the player to the MoveHere
+                    MoveHere.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
             }
 
-            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            //Moves up if in front of a box that doesn't have a box on top of it
+            //Checks for the up input
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f && PlayerProps.InFront)
             {
+                //Move player to MoveHere
                 MoveHere.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
             }
         }
