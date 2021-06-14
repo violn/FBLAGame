3fdb3b08fa8beed5a68a12eaa9f979b6caaeda2d
niol <32370882+violn@users.noreply.github.com>
2021-06-14 12:02:11 -0700

    Update ChangeScenes.cs

--- Scripts/Level/Stage/ChangeScenes.cs
+++ Scripts/Level/Stage/ChangeScenes.cs
@@ -8,54 +8,53 @@ public class ChangeScenes : MonoBehaviour
 
     private void Restart()
     {
-        if (Fail.inFail)
+        if (!FinishLevel.inResults)
         {
             Lives.liveRemaining--;
         }
+
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
     private void Next()
     {
         Lives.liveRemaining = 3;
-        Lives.resetLives = true;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     private void ToMenue()
     {
         Lives.liveRemaining = 3;
-        Lives.resetLives = true;
         SceneManager.LoadScene(1);
     }
 
-    public void StartRestart()
+    private void StartRestart()
     {
         iTween.MoveTo(GlobalRefLevel.globalObj.sidePanelRight,
             iTween.Hash(
-                "position", new Vector3(500f, -550f) + GlobalRef.globalVariable.localPosition,
+                "position", new Vector3(500f, -550f) + GlobalVariables.localPosition,
                 "time", 1f));
 
         iTween.MoveTo(GlobalRefLevel.globalObj.sidePanelLeft,
             iTween.Hash(
-                "position", new Vector3(-500f, -1000f) + GlobalRef.globalVariable.localPosition,
+                "position", new Vector3(-500f, -1000f) + GlobalVariables.localPosition,
                 "time", 1f,
                 "oncompletetarget", gameObject,
                 "oncomplete", "Restart"));
     }
 
-    public void StartNext()
+    private void StartNext()
     {
         if (GlobalRefLevel.levelProperties.currentLevel != SceneManager.sceneCountInBuildSettings - 2)
         {
             iTween.MoveTo(GlobalRefLevel.globalObj.sidePanelRight,
                 iTween.Hash(
-                    "position", new Vector3(500f, -550f) + GlobalRef.globalVariable.localPosition,
+                    "position", new Vector3(500f, -550f) + GlobalVariables.localPosition,
                     "time", 1f));
 
             iTween.MoveTo(GlobalRefLevel.globalObj.sidePanelLeft,
                 iTween.Hash(
-                    "position", new Vector3(-500f, -1000f) + GlobalRef.globalVariable.localPosition,
+                    "position", new Vector3(-500f, -1000f) + GlobalVariables.localPosition,
                     "time", 1f,
                     "oncompletetarget", gameObject,
                     "oncomplete", "Next"));
@@ -66,15 +65,15 @@ public class ChangeScenes : MonoBehaviour
         }
     }
 
-    public void StartMenue()
+    private void StartMenue()
     {
         iTween.MoveTo(GlobalRefLevel.globalObj.sidePanelRight,
             iTween.Hash(
-                "position", new Vector3(500f, -550f) + GlobalRef.globalVariable.localPosition,
+                "position", new Vector3(500f, -550f) + GlobalVariables.localPosition,
                 "time", 1f));
 
         iTween.MoveTo(GlobalRefLevel.globalObj.sidePanelLeft,
-            iTween.Hash("position", new Vector3(-500f, -1000f) + GlobalRef.globalVariable.localPosition,
+            iTween.Hash("position", new Vector3(-500f, -1000f) + GlobalVariables.localPosition,
             "time", 1f,
             "oncompletetarget", gameObject,
             "oncomplete", "ToMenue"));
