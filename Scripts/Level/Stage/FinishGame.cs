14e62f3970de2e56cc17b2aca4ea55f65becdaf4
niol <32370882+violn@users.noreply.github.com>
2021-06-14 12:03:39 -0700

    Update FinishGame.cs

--- Scripts/Level/Stage/FinishGame.cs
+++ Scripts/Level/Stage/FinishGame.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,9 +8,9 @@ public class FinishGame : MonoBehaviour
     public GameObject leaderboard;
     public GameObject credits;
     public GameObject results;
-    public Text data;
-    public Text dataFG;
-    public static bool finishGame = false;
+    public Text dataTextFG;
+    public Text dataTextBG;
+    public static bool finishGame;
     public static int totalScore;
 
     public void Start()
@@ -19,80 +20,71 @@ public class FinishGame : MonoBehaviour
 
     public void Update()
     {
-        if (finishGame == true)
+        if (finishGame)
         {
             finishGame = false;
-            foreach (int s in GlobalRef.levelScores)
+            foreach (int s in FinishLevel.levelScores)
             {
                 totalScore += s;
             }
 
-            if (ScoreHandler.scoreKeeper != null && !ScoreHandler.scoreKeeper.ContainsKey(NameEntry.playerName))
+            if(NameEntry.playerName == null)
+            {
+                NameEntry.playerName = "DefaultDan";
+            }
+
+            if (!ScoreHandler.scoreKeeper.ContainsKey(NameEntry.playerName))
             {
                 ScoreHandler.scoreKeeper.Add(NameEntry.playerName, totalScore);
             }
             else
             {
-                int existing_score = ScoreHandler.scoreKeeper[NameEntry.playerName];
-
-                if (existing_score < totalScore)
-                {
-                    ScoreHandler.scoreKeeper[NameEntry.playerName] = totalScore;
-                }
+                ScoreHandler.scoreKeeper[NameEntry.playerName] = ScoreHandler.scoreKeeper[NameEntry.playerName] < totalScore ? totalScore : ScoreHandler.scoreKeeper[NameEntry.playerName];
             }
 
             WriteDownScores();
-            MoveThingsIn();
+            MoveCreditsIn();
         }
     }
 
-    public void MoveThingsIn()
+    private void MoveCreditsIn()
     {
         iTween.MoveTo(results,
             iTween.Hash(
-                "position", new Vector3(-500f, -1000f) + GlobalRef.globalVariable.localPosition,
+                "position", new Vector3(-500f, -1000f) + GlobalVariables.localPosition,
                 "time", 1f));
 
         iTween.MoveTo(leaderboard,
             iTween.Hash(
-                "position", new Vector3(500f, 0f) + GlobalRef.globalVariable.localPosition,
+                "position", new Vector3(500f, 0f) + GlobalVariables.localPosition,
                 "time", 1f,
                 "delay", .5));
 
         iTween.MoveTo(credits,
             iTween.Hash(
-                "position", new Vector3(-500f, 0f) + GlobalRef.globalVariable.localPosition,
+                "position", new Vector3(-500f, 0f) + GlobalVariables.localPosition,
                 "time", 1f));
     }
 
-    public void WriteDownScores()
+    private void WriteDownScores()
     {
-        List<int> scores = new List<int>();
         Dictionary<string, int> sorted_scores = new Dictionary<string, int>();
-
-        foreach (KeyValuePair<string, int> entry in ScoreHandler.scoreKeeper)
-        {
-            scores.Add(entry.Value);
-        }
-
+        List<int> scores = (from KeyValuePair<string, int> entry in ScoreHandler.scoreKeeper
+                            select entry.Value).ToList();
         scores.Sort();
         scores.Reverse();
-
-        foreach (int s in scores)
+        foreach (var entry in from int s in scores
+                              from KeyValuePair<string, int> entry in ScoreHandler.scoreKeeper
+                              where entry.Value == s && sorted_scores != null && !sorted_scores.ContainsKey(entry.Key)
+                              select entry)
         {
-            foreach (KeyValuePair<string, int> entry in ScoreHandler.scoreKeeper)
-            {
-                if (entry.Value == s && sorted_scores != null && !sorted_scores.ContainsKey(entry.Key))
-                {
-                    sorted_scores.Add(entry.Key, entry.Value);
-                }
-            }
+            sorted_scores.Add(entry.Key, entry.Value);
         }
 
         foreach (KeyValuePair<string, int> entry in sorted_scores)
         {
-            data.text += entry.Key + " " + entry.Value + "\n";
-            dataFG.text += entry.Key + " " + entry.Value + "\n";
+            dataTextBG.text += $"{entry.Key} {entry.Value}\n";
+            dataTextFG.text += $"{entry.Key} {entry.Value}\n";
         }
 
         ScoreHandler.Save();
