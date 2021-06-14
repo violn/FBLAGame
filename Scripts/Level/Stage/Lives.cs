e0ace6eb2f8dccd9556419b0d2da0d3e119f22f4
niol <32370882+violn@users.noreply.github.com>
2021-06-14 12:04:30 -0700

    Update Lives.cs

--- Scripts/Level/Stage/Lives.cs
+++ Scripts/Level/Stage/Lives.cs
@@ -4,35 +4,31 @@ using UnityEngine.UI;
 public class Lives : MonoBehaviour
 {
     public static int liveRemaining = 3;
-    public static bool resetLives = false;
-    private static Color gray = new Color(.5f, .5f, .5f, 1f);
     public static Color32 lifeColor = new Color32(179, 77, 77, 255);
     public RawImage life1;
     public RawImage life2;
     public RawImage life3;
 
-    public void Start()
-    {
-        if (resetLives)
-        {
-            life1.color = lifeColor;
-            life2.color = lifeColor;
-            life3.color = lifeColor;
-            resetLives = false;
-        }
-    }
-
-    public void Update()
+    private void Start()
     {
         switch (liveRemaining)
         {
             case 1:
-                life2.color = gray;
-                life3.color = gray;
+                life1.color = lifeColor;
+                life2.color = Color.gray;
+                life3.color = Color.gray;
                 break;
 
             case 2:
-                life3.color = gray;
+                life1.color = lifeColor;
+                life2.color = lifeColor;
+                life3.color = Color.gray;
+                break;
+
+            case 3:
+                life1.color = lifeColor;
+                life2.color = lifeColor;
+                life3.color = lifeColor;
                 break;
         }
     }
