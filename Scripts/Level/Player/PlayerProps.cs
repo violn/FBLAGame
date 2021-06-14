965de22e0555f5d0dd272725611d9a94c337960c
niol <32370882+violn@users.noreply.github.com>
2021-06-14 12:01:33 -0700

    Update PlayerProps.cs

--- Scripts/Level/Player/PlayerProps.cs
+++ Scripts/Level/Player/PlayerProps.cs
@@ -1,105 +1,39 @@
-//Player properties that can be referenced by other classes
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerProps : MonoBehaviour
 {
-    public int depth;
-    public int currentDepth;
-    public bool holdingSomething;
-    public bool grabState;
-    public bool moving;
-    public bool finishedScalingDown;
-    public bool finishedScalingUp;
-    public readonly float moveTime = .25f;
-    public readonly float shrinkMove = .1875f;
-    public readonly float grabMove = .6875f;
-    public Renderer playerRend;
-    private Color defaultColor;
+    public static List<LayerMask> depth = new List<LayerMask>();
+    public List<LayerMask> d = new List<LayerMask>();
+    public static GameObject player;
+    public static int currentDepth;
+    public static bool still;
 
-    private void Start()
+    private void Awake()
     {
-        holdingSomething = false;
-        grabState = false;
-        moving = false;
-        finishedScalingDown = true;
-        finishedScalingUp = true;
-        depth = 0;
+        depth = d;
         currentDepth = 0;
-        defaultColor = playerRend.material.color;
+        player = gameObject;
+        still = false;
     }
 
-    private void Update()
+    private void DoneScaling()
     {
-        if (depth != currentDepth)
-        {
-            if (finishedScalingUp && finishedScalingDown)
-            {
-                if (currentDepth == 0 && !ChangedToDepth1())
-                {
-                    playerRend.material.color *= new Color(.5f, .5f, .5f);
-                    finishedScalingDown = false;
-                    ScaleDown();
-                }
-
-                if (currentDepth == 1 && !ChangedToDepth0())
-                {
-                    playerRend.material.color *= new Color(2f, 2f, 2f);
-                    finishedScalingUp = false;
-                    ScaleUp();
-                }
-            }
-        }
+        still = true;
     }
 
-    private bool ChangedToDepth1()
+    public static void ChangeScale()
     {
-        return playerRend.material.color.r == defaultColor.r * .5f && finishedScalingDown;
-    }
-
-    private bool ChangedToDepth0()
-    {
-        return playerRend.material.color == defaultColor && finishedScalingUp;
-    }
-
-    private void DoneScalingDown()
-    {
-        finishedScalingDown = true;
-        currentDepth = 1;
-    }
-
-    private void DoneScalingUp()
-    {
-        finishedScalingUp = true;
-        currentDepth = 0;
-    }
-
-    private void ScaleDown()
-    {
-        iTween.MoveAdd(gameObject,
+        still = false;
+        player.GetComponent<Renderer>().material.color *= currentDepth == 0 ? new Color(.5f, .5f, .5f) : new Color(2f, 2f, 2f);
+        iTween.ScaleTo(player,
             iTween.Hash(
-                "amount", new Vector3(0f, -shrinkMove, 0f),
-                "time", moveTime));
-
-        iTween.ScaleTo(gameObject,
-            iTween.Hash(
-                "scale", new Vector3(.375f, .375f, 0f),
-                "time", moveTime,
-                "oncompletetarget", gameObject,
-                "oncomplete", "DoneScalingDown"));
+                "scale", currentDepth == 0 ? new Vector3(.375f, .375f) : new Vector3(.75f, .75f),
+                "time", Move.moveTime,
+                "oncompletetarget", player,
+                "oncomplete", "DoneScaling"));
+        currentDepth = currentDepth == 0 ? 1 : 0;
     }
 
-    private void ScaleUp()
-    {
-        iTween.MoveAdd(gameObject,
-            iTween.Hash(
-                "amount", new Vector3(0f, shrinkMove, 0f),
-                "time", moveTime));
-
-        iTween.ScaleTo(gameObject,
-            iTween.Hash(
-                "scale", new Vector3(.75f, .75f, 0f),
-                "time", moveTime,
-                "oncompletetarget", gameObject,
-                "oncomplete", "DoneScalingUp"));
-    }
+    public static int GetOppositeDepth() => currentDepth == 0 ? 1 : 0;
 }
