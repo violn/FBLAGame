104c45caf0f09788fa0e7fa1de6aeae070fe70ca
niol <32370882+violn@users.noreply.github.com>
2021-06-14 10:59:22 -0700

    Update GlobalMethods.cs

--- Scripts/Global/GlobalMethods.cs
+++ Scripts/Global/GlobalMethods.cs
@@ -2,32 +2,32 @@ using UnityEngine;
 
 public class GlobalMethods : MonoBehaviour
 {
-    public void SidePanelsMoveIn()
+    public static void SidePanelsMoveIn()
     {
         iTween.MoveTo(GlobalRefLevel.globalObj.sidePanelLeft,
             iTween.Hash(
-                "position", new Vector3(GlobalRefLevel.globalObj.sidePanelLeft.transform.position.x, -450 + GlobalRef.globalVariable.localPosition.y),
+                "position", new Vector3(GlobalRefLevel.globalObj.sidePanelLeft.transform.position.x, -450 + GlobalVariables.localPosition.y),
                 "easeType", "easeOutBack",
                 "time", .5f));
 
         iTween.MoveTo(GlobalRefLevel.globalObj.sidePanelRight,
             iTween.Hash(
-                "position", new Vector3(GlobalRefLevel.globalObj.sidePanelRight.transform.position.x, 450 + GlobalRef.globalVariable.localPosition.y),
+                "position", new Vector3(GlobalRefLevel.globalObj.sidePanelRight.transform.position.x, 450 + GlobalVariables.localPosition.y),
                 "easeType", "easeOutBack",
                 "time", .5f));
     }
 
-    public void SidePanelsMoveOut()
+    public static void SidePanelsMoveOut()
     {
         iTween.MoveTo(GlobalRefLevel.globalObj.sidePanelLeft,
             iTween.Hash(
-                "position", new Vector3(GlobalRefLevel.globalObj.sidePanelLeft.transform.position.x, -600 + GlobalRef.globalVariable.localPosition.y),
+                "position", new Vector3(GlobalRefLevel.globalObj.sidePanelLeft.transform.position.x, -600 + GlobalVariables.localPosition.y),
                 "easeType", "easeOutBack",
                 "time", .5f));
 
         iTween.MoveTo(GlobalRefLevel.globalObj.sidePanelRight,
             iTween.Hash(
-                "position", new Vector3(GlobalRefLevel.globalObj.sidePanelRight.transform.position.x, 600 + GlobalRef.globalVariable.localPosition.y),
+                "position", new Vector3(GlobalRefLevel.globalObj.sidePanelRight.transform.position.x, 600 + GlobalVariables.localPosition.y),
                 "easeType", "easeOutBack",
                 "time", .5f));
     }
