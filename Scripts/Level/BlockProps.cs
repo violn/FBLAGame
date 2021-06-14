4bbb6e34a069d2cb74c197d829503e8be58c1b22
niol <32370882+violn@users.noreply.github.com>
2021-06-14 11:58:02 -0700

    Add files via upload

---
+++ Scripts/Level/BlockProps.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BlockProps : MonoBehaviour
+{
+    public enum Type
+    {
+        regular,
+        final
+    }
+
+    public Type blockType;
+    public static Vector3 defaultPosition;
+
+    public void Awake()
+    {
+        defaultPosition = gameObject.transform.position;
+    }
+}
